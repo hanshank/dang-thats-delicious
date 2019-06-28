5eab38cb94062cae0897c23f73dd04c002350fb2
Hans Martin <hansmhank@gmail.com>
2019-06-28 15:28:17 -0700

    finished geo location, aka module 5

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,10 +1,9 @@
-exports.myMiddleware = (req, res, next) => {
-  req.name = 'Wes';
-  next();
-};
+const mongoose = require('mongoose');
+
+const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
-  console.log(req.name);
+  req.flash('error', `Something happened`);
   res.render('index');
 };
 
@@ -12,6 +11,32 @@ exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' });
 };
 
-exports.createStore = (req, res) => {
-  res.json(req.body);
+exports.createStore = async (req, res) => {
+  const store = await new Store(req.body).save();
+  req.flash('success', `Succesfully created ${store.name}. Care to leave a review?`);
+  res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = async (req, res) => {
+  const stores = await Store.find();
+  res.render('stores', { title: 'Stores', stores });
+};
+
+exports.editStore = async (req, res) => {
+  // res.json(req.params);
+  const store = await Store.findOne({ _id: req.params.id });
+  res.render('editStore', { title: `Edit ${store.name}`, store });
+};
+
+exports.updateStore = async (req, res) => {
+  req.body.location.type = 'Point';
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, // returns the new store instead of the old one
+    runValidators: true,
+  }).exec();
+  req.flash(
+    'success',
+    `Succesfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View store -></a>`
+  );
+  res.redirect(`/stores/${store._id}/edit`);
 };
--- data/load-sample-data.js
+++ data/load-sample-data.js
@@ -1,8 +1,9 @@
-require('dotenv').config({ path: __dirname + '/../variables.env' });
+require('dotenv').config({ path: `${__dirname}/../variables.env` });
 const fs = require('fs');
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE);
+
+mongoose.connect(process.env.DEV_DATABASE);
 mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
 
 // import all of our models - they need to be imported only once
@@ -10,10 +11,9 @@ const Store = require('../models/Store');
 const Review = require('../models/Review');
 const User = require('../models/User');
 
-
-const stores = JSON.parse(fs.readFileSync(__dirname + '/stores.json', 'utf-8'));
-const reviews = JSON.parse(fs.readFileSync(__dirname + '/reviews.json', 'utf-8'));
-const users = JSON.parse(fs.readFileSync(__dirname + '/users.json', 'utf-8'));
+const stores = JSON.parse(fs.readFileSync(`${__dirname}/stores.json`, 'utf-8'));
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
 
 async function deleteData() {
   console.log('😢😢 Goodbye Data...');
@@ -31,8 +31,10 @@ async function loadData() {
     await User.insertMany(users);
     console.log('👍👍👍👍👍👍👍👍 Done!');
     process.exit();
-  } catch(e) {
-    console.log('\n👎👎👎👎👎👎👎👎 Error! The Error info is below but if you are importing sample data make sure to drop the existing database first with.\n\n\t npm run blowitallaway\n\n\n');
+  } catch (e) {
+    console.log(
+      '\n👎👎👎👎👎👎👎👎 Error! The Error info is below but if you are importing sample data make sure to drop the existing database first with.\n\n\t npm run blowitallaway\n\n\n'
+    );
     console.log(e);
     process.exit();
   }
